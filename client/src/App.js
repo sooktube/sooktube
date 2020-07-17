dbefb505df03bce8906cf406e7c960e0c793ca6a
JangJua <jua0610@naver.com>
2020-07-17 16:55:45 +0900

    modify: 회원가입 문구 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {Router, Route, Switch, Redirect } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import { history } from './helpers';
-import { alertActions } from './actions';
 import './reset.css';
 
 import Main from "./pages/Main";
