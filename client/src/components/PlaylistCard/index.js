1df864275d5951528c1b348ef2ddbd419ee519fa
JangJua <jua0610@naver.com>
2020-07-21 14:08:11 +0900

    add: Card 컴포넌트 추가

--- client/src/components/PlaylistCard/index.js
+++ client/src/components/PlaylistCard/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import './card.scss';
+import {CardDate, CardDesc, CardLike, CardTitle, CardWrapper, UtilityInfo} from "./style";
+import {FaHeart} from "react-icons/all";
 
 function PlayListCard() {
 
+    return (
+        <>
+            <CardWrapper>
+                <CardTitle>
+                    <h3>SPRING FEVER</h3>
+                    <hr/>
+                    <div className="intro">Yllamco laboris nisi ut aliquip ex ea commodo.</div>
+                </CardTitle>
+                <CardDesc>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim.
+                </CardDesc>
+                <UtilityInfo>
+                    <UtilityInfo>
+                        <CardDate> 2020.07.15 </CardDate>
+                        <CardLike> 3 <FaHeart/> </CardLike>
+                    </UtilityInfo>
+                </UtilityInfo>
+                <div className="gradient-overlay"></div>
+                <div className="color-overlay"></div>
+            </CardWrapper>
+        </>
+    );
 }
 
 export default PlayListCard;
--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from "../../components/Header";
 import { MainBackground } from "./style";
+import PlayListCard from "../../components/PlaylistCard";
 
 function Main() {
     return (
         <MainBackground>
             <Header/>
+            <PlayListCard/>
         </MainBackground>
     )
 }
