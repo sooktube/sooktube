7b66d0a339a392a272753bcd04c2f4b502aa9133
JangJua <jua0610@naver.com>
2020-07-21 18:05:40 +0900

    modify: dummy data와 연결

--- client/src/components/PlaylistCard/CardShelf/index.js
+++ client/src/components/PlaylistCard/CardShelf/index.js
@@ -2,62 +2,52 @@ import React from 'react';
 import {ShelfWrapper, CardWrapper, Shelf, CardImg, CardInfo, CardTitle, CardHeart, ShelfBottom} from "./style";
 
 function CardShelf() {
+    const playlists = [
+        {
+            listName: "재생목록1",
+            listDesc: null,
+            listLike: 7,
+            thumbnail: "https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"
+
+        },
+        {
+            listName: "재생목록1",
+            listDesc: null,
+            listLike: 7,
+            thumbnail: "https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"
+        },
+        {
+            listName: "재생목록1",
+            listDesc: null,
+            listLike: 7,
+            thumbnail: "https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"
+        },
+        {
+            listName: "재생목록1",
+            listDesc: null,
+            listLike: 7,
+            thumbnail: "https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"
+        },
+        {
+            listName: "재생목록1",
+            listDesc: null,
+            listLike: 7,
+            thumbnail: "https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"
+        }
+
+    ];
     return (
         <>
         <ShelfWrapper>
-            <CardWrapper>
-              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"/>
-              <CardInfo>
-                  <CardTitle> 재생목록 1 </CardTitle>
-              </CardInfo>
-              <CardHeart/>
-            </CardWrapper>
-            <CardWrapper>
-              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-DQPY/20favorite-book-covers-slide-DQPY-slide.jpg"/>
-              <CardInfo>
-                  <CardTitle> 재생목록 1 </CardTitle>
-              </CardInfo>
-              <CardHeart/>
-            </CardWrapper>
-            <CardWrapper>
-              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-QNUF/20favorite-book-covers-slide-QNUF-slide.jpg"/>
-              <CardInfo>
-                  <CardTitle> 재생목록 1 </CardTitle>
-              </CardInfo>
-              <CardHeart/>
-            </CardWrapper>
-            <CardWrapper>
-              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-U60O/20favorite-book-covers-slide-U60O-slide.jpg"/>
-              <CardInfo>
-                  <CardTitle> 재생목록 1 </CardTitle>
-              </CardInfo>
-              <CardHeart/>
-            </CardWrapper>
-            <CardWrapper>
-                <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"/>
-                <CardInfo>
-                    <CardTitle> 재생목록 1 </CardTitle>
-                </CardInfo>
-                <CardHeart/>
-            </CardWrapper>
-            <CardWrapper>
-                <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"/>
-                <CardInfo>
-                    <CardTitle> 재생목록 1 </CardTitle>
-                </CardInfo>
-                <CardHeart/>
-            </CardWrapper>
-            <CardWrapper>
-                <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"/>
-                <CardInfo>
-                    <CardTitle> 재생목록 1 </CardTitle>
-                </CardInfo>
-                <CardHeart/>
-            </CardWrapper>
-
-
-
-
+            {playlists.map(list => (
+                <CardWrapper>
+                    <CardImg src={list.thumbnail}/>
+                    <CardInfo>
+                        <CardTitle> {list.listName} </CardTitle>
+                    </CardInfo>
+                    <CardHeart/>
+                </CardWrapper>
+            ))}
             <Shelf/>
             <ShelfBottom/>
         </ShelfWrapper>
