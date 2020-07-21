76479537fda5d3858e353954867168b420c19799
JangJua <jua0610@naver.com>
2020-07-21 16:26:40 +0900

    add: Card UI 추가

--- client/src/components/PlaylistCard/index.js
+++ client/src/components/PlaylistCard/index.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import './card.scss';
 import {FaHeart} from "react-icons/all";
+import {PlaylistWrapper, CardWrapper, CardShelf, CardImg, CardInfo, CardTitle, CardHeart} from "./style";
 
 function PlaylistCard() {
-
+    const a = 'a';
     return (
-      <>
-          <div className="books">
-                <img src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"/>
-                <img src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-DQPY/20favorite-book-covers-slide-DQPY-slide.jpg"/>
-                <img src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-QNUF/20favorite-book-covers-slide-QNUF-slide.jpg"/>
-                <img src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-U60O/20favorite-book-covers-slide-U60O-slide.jpg"/>
-                <div className="shelf"></div>
-          </div>
-      </>
+        <PlaylistWrapper>
+          <CardWrapper>
+              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-ORTM/20favorite-book-covers-slide-ORTM-slide.jpg"/>
+              <CardInfo>
+                  <CardTitle> 재생목록 1 </CardTitle>
+              </CardInfo>
+              <CardHeart/>
+          </CardWrapper>
+          <CardWrapper>
+              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-DQPY/20favorite-book-covers-slide-DQPY-slide.jpg"/>
+              <CardInfo>
+                  <CardTitle> 재생목록 1 </CardTitle>
+              </CardInfo>
+              <CardHeart/>
+          </CardWrapper>
+          <CardWrapper>
+              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-QNUF/20favorite-book-covers-slide-QNUF-slide.jpg"/>
+              <CardInfo>
+                  <CardTitle> 재생목록 1 </CardTitle>
+              </CardInfo>
+              <CardHeart/>
+          </CardWrapper>
+          <CardWrapper>
+              <CardImg src="https://graphics8.nytimes.com/images/2012/12/19/books/20favorite-book-covers-slide-U60O/20favorite-book-covers-slide-U60O-slide.jpg"/>
+              <CardInfo>
+                  <CardTitle> 재생목록 1 </CardTitle>
+              </CardInfo>
+              <CardHeart/>
+          </CardWrapper>
+            <CardShelf/>
+        </PlaylistWrapper>
     );
 }
 
