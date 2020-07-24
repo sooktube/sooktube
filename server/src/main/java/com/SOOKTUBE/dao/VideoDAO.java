da377e2a680c52251e46ea38ccfcf8ac8866c29b
전하정 <vane815@naver.com>
2020-07-24 16:42:07 +0900

    API: get videoURL by videoID/username

--- server/src/main/java/com/SOOKTUBE/dao/VideoDAO.java
+++ server/src/main/java/com/SOOKTUBE/dao/VideoDAO.java
@@ -1,9 +1,13 @@
 package com.SOOKTUBE.dao;
 
+import java.util.List;
+
 import com.SOOKTUBE.model.VideoDTO;
 
 public interface VideoDAO {
 	
 	int newVideo(VideoDTO param) throws Exception;
+	String getURLfromVideoID(int videoID) throws Exception;
+	List<String> getURLfromUsername(String username) throws Exception;
 
 }
