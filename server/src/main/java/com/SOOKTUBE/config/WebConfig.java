6786f83451f1b870f652ec3c60b5294bc7c872be
전하정 <vane815@naver.com>
2020-07-31 14:29:27 +0900

    CORS Policy configuration(allow localhost 9000)

--- server/src/main/java/com/SOOKTUBE/config/WebConfig.java
+++ server/src/main/java/com/SOOKTUBE/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	
+	//cors policy configuration
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
