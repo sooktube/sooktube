de5be8db83a96056475b6fb4605d41c9c5953b59
JangJua <jua0610@naver.com>
2020-08-19 22:49:52 +0900

    merge: 유저 마이페이지 추가

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -120,113 +120,113 @@ function Register() {
         const filename = 'profile' + time + event.target.files[0].name;
         setUser({...user, profilepic : filename });
         userService.getProfilePicUploadUrl(filename)
-        .then(response => {
-            userService.UploadUserProfilePic({
-                uploadURL : response,
-                imageFile : imageFile
-            }).then(() => {
-                userService.getProfileUrlByProfilepic(filename)
-                .then(response => {
-                    userImage = response;
-                    setImageURL(userImage);
+            .then(response => {
+                userService.UploadUserProfilePic({
+                    uploadURL : response,
+                    imageFile : imageFile
+                }).then(() => {
+                    userService.getProfileUrlByProfilepic(filename)
+                        .then(response => {
+                            userImage = response;
+                            setImageURL(userImage);
+                        })
                 })
             })
-        })
     }
-   
-    
+
+
     return (
         <S.MainBackground>
-         <S.RegisterForm className="register">
-            <form name="form" onSubmit={handleSubmit}>
-                <S.RegisterLogo> SOOKTUBE </S.RegisterLogo> 
-                <S.LabelBox>
-                <S.Label img={imageURL} >
+            <S.RegisterForm className="register">
+                <form name="form" onSubmit={handleSubmit}>
+                    <S.RegisterLogo> SOOKTUBE </S.RegisterLogo>
+                    <S.LabelBox>
+                        <S.Label img={imageURL} >
                             <S.UploadInput type="file" onChange={fileSelect}/>
-                </S.Label>
-                <S.AddText>Add Profile Photo</S.AddText>
-                </S.LabelBox>
-                <S.FormGroupA>
-                    <S.LabelName> Email </S.LabelName>
-                    <S.InputR type="text"
-                            name="userID"
-                            id="ruserID"
-                            value={user.userID}
-                            onChange={validateUserID}
-                            onBlur={checkDuplicateUserID}
-                            className={'form-control' + ((submitted && !user.userID) ||
-                            (user.userID && (!validate.userID || isDuplicate.userID)) ? ' is-invalid' : '')}/>
-                    {(submitted && !user.userID) &&
-                    <S.InvalidFeedback> 필수 정보입니다. </S.InvalidFeedback>
-                    }
-                    {validate.userID === false &&
-                    <S.InvalidFeedback> 올바른 이메일 형식으로 입력해주세요. </S.InvalidFeedback>
-                    }
-                    {isDuplicate.userID === true &&
-                    <S.InvalidFeedback> 사용 중인 이메일입니다. </S.InvalidFeedback>
-                    }
-                </S.FormGroupA>
-                <S.FormGroupB>
-                    <S.LabelName> Nickname </S.LabelName>
-                    <S.LabelName2> 다른 유저와 중복되지 않는 별명으로 입력해주세요. (2자 ~ 10자) </S.LabelName2>
-                    <S.InputR
-                        type="text"
-                        name="username"
-                        id="rusername"
-                        value={user.username}
-                        onChange={validateUsername}
-                        onBlur={checkDuplicateUsername}
-                        className={'form-control' + ((submitted && !user.username) ||
+                        </S.Label>
+                        <S.AddText>Add Profile Photo</S.AddText>
+                    </S.LabelBox>
+                    <S.FormGroupA>
+                        <S.LabelName> Email </S.LabelName>
+                        <S.InputR type="text"
+                                  name="userID"
+                                  id="ruserID"
+                                  value={user.userID}
+                                  onChange={validateUserID}
+                                  onBlur={checkDuplicateUserID}
+                                  className={'form-control' + ((submitted && !user.userID) ||
+                                  (user.userID && (!validate.userID || isDuplicate.userID)) ? ' is-invalid' : '')}/>
+                        {(submitted && !user.userID) &&
+                        <S.InvalidFeedback> 필수 정보입니다. </S.InvalidFeedback>
+                        }
+                        {validate.userID === false &&
+                        <S.InvalidFeedback> 올바른 이메일 형식으로 입력해주세요. </S.InvalidFeedback>
+                        }
+                        {isDuplicate.userID === true &&
+                        <S.InvalidFeedback> 사용 중인 이메일입니다. </S.InvalidFeedback>
+                        }
+                    </S.FormGroupA>
+                    <S.FormGroupB>
+                        <S.LabelName> Nickname </S.LabelName>
+                        <S.LabelName2> 다른 유저와 중복되지 않는 별명으로 입력해주세요. (2자 ~ 10자) </S.LabelName2>
+                        <S.InputR
+                            type="text"
+                            name="username"
+                            id="rusername"
+                            value={user.username}
+                            onChange={validateUsername}
+                            onBlur={checkDuplicateUsername}
+                            className={'form-control' + ((submitted && !user.username) ||
                             (user.username && (!validate.username || isDuplicate.username)) ? ' is-invalid' : '')}/>
-                    {submitted && !user.username &&
-                    <S.InvalidFeedback>필수 정보입니다.</S.InvalidFeedback>
-                    }
-                    {validate.username === false &&
-                    <S.InvalidFeedback> 2자에서 16자 사이로 입력해주세요. </S.InvalidFeedback>
-                    }
-                    {isDuplicate.username === true &&
-                    <S.InvalidFeedback> 사용 중인 별명입니다. </S.InvalidFeedback>
-                    }
-                </S.FormGroupB>
-                <S.FormGroupC>
-                    <S.LabelName>Password</S.LabelName>
-                    <S.LabelName2> 8자 이상 입력해주세요. </S.LabelName2>
-                    <S.InputR type="password"
-                           name="password"
-                           id="rpassword"
-                           value={user.password}
-                           onChange={validatePassword}
-                           className={'form-control' + (submitted && !user.password ? ' is-invalid' : '')}/>
-                    {submitted && !user.password &&
-                    <S.InvalidFeedback>필수 정보입니다.</S.InvalidFeedback>
-                    }
-                    {validate.password === false &&
-                    <S.InvalidFeedback> 8자 이상 입력해주세요. </S.InvalidFeedback>
-                    }
-                </S.FormGroupC>
-                <S.FormGroupD>
-                    <S.LabelName>Re Enter the Password</S.LabelName>
-                    <S.InputR type="password"
-                           name="passwordChk"
-                           id="rpassword"
-                           value={user.passwordChk}
-                           onChange={handleChange}
-                           className={'form-control' + (submitted && !user.passwordChk ? ' is-invalid' : '')}/>
-                    {submitted && !user.passwordChk &&
-                    <S.InvalidFeedback>필수 정보입니다. </S.InvalidFeedback>
-                    }
-                    {submitted && (user.passwordChk !== user.password) &&
-                    <S.InvalidFeedback> 입력된 비밀번호와 다릅니다. </S.InvalidFeedback>
-                    }
-                </S.FormGroupD>
-                <S.FormGroupE>
-                    <S.Rsubmit>
-                        SIGN UP
-                        {registering && <span className="spinner-border spinner-border-sm mr-1" style={{margin: '0 0 0 5px'}}/>}
-                    </S.Rsubmit>
-                </S.FormGroupE>
-            </form>
-         </S.RegisterForm>
+                        {submitted && !user.username &&
+                        <S.InvalidFeedback>필수 정보입니다.</S.InvalidFeedback>
+                        }
+                        {validate.username === false &&
+                        <S.InvalidFeedback> 2자에서 16자 사이로 입력해주세요. </S.InvalidFeedback>
+                        }
+                        {isDuplicate.username === true &&
+                        <S.InvalidFeedback> 사용 중인 별명입니다. </S.InvalidFeedback>
+                        }
+                    </S.FormGroupB>
+                    <S.FormGroupC>
+                        <S.LabelName>Password</S.LabelName>
+                        <S.LabelName2> 8자 이상 입력해주세요. </S.LabelName2>
+                        <S.InputR type="password"
+                                  name="password"
+                                  id="rpassword"
+                                  value={user.password}
+                                  onChange={validatePassword}
+                                  className={'form-control' + (submitted && !user.password ? ' is-invalid' : '')}/>
+                        {submitted && !user.password &&
+                        <S.InvalidFeedback>필수 정보입니다.</S.InvalidFeedback>
+                        }
+                        {validate.password === false &&
+                        <S.InvalidFeedback> 8자 이상 입력해주세요. </S.InvalidFeedback>
+                        }
+                    </S.FormGroupC>
+                    <S.FormGroupD>
+                        <S.LabelName>Re Enter the Password</S.LabelName>
+                        <S.InputR type="password"
+                                  name="passwordChk"
+                                  id="rpassword"
+                                  value={user.passwordChk}
+                                  onChange={handleChange}
+                                  className={'form-control' + (submitted && !user.passwordChk ? ' is-invalid' : '')}/>
+                        {submitted && !user.passwordChk &&
+                        <S.InvalidFeedback>필수 정보입니다. </S.InvalidFeedback>
+                        }
+                        {submitted && (user.passwordChk !== user.password) &&
+                        <S.InvalidFeedback> 입력된 비밀번호와 다릅니다. </S.InvalidFeedback>
+                        }
+                    </S.FormGroupD>
+                    <S.FormGroupE>
+                        <S.Rsubmit>
+                            SIGN UP
+                            {registering && <span className="spinner-border spinner-border-sm mr-1" style={{margin: '0 0 0 5px'}}/>}
+                        </S.Rsubmit>
+                    </S.FormGroupE>
+                </form>
+            </S.RegisterForm>
         </S.MainBackground>
     );
 }
