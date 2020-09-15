236e23ab1c6fc6d6a12f63d6955ca09fa4677d81
JangJua <jua0610@naver.com>
2020-09-16 04:11:56 +0900

    add: 스켈레톤 UI 추가

--- client/src/components/playlist/SearchVideo/index.js
+++ client/src/components/playlist/SearchVideo/index.js
@@ -1,27 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import * as S from './style';
-import {playlistService} from "../../../services";
+import debounce from 'lodash.debounce';
+import { playlistService } from "../../../services";
 import VideoList from "../VideoList";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import RecommendVideoList from "../RecommendVideoList";
+import FallbackVideoList from "../FallbackVideoList";
 
 function SearchVideo({listID}) {
     const [keyword, setKeyword] = useState('');
     const [searchResult, setSearchResult] = useState(null);
+    const [loading, setLoading] = useState(true);
     const username = useSelector(state => state.authentication.username);
 
     function handleChange(e) {
         const { value } = e.target;
+        setLoading(true);
         setKeyword(value);
     }
 
-    useEffect(() =>{
+    const updateSearch = useCallback(() => {
         if(keyword) {
-            playlistService.searchVideoByTitle({keyword, listID, username, orderBy:'like',limit:'10',offset:0}) 
+            playlistService.searchVideoByTitle({keyword, listID, username, orderBy:'like',limit:'10',offset:0})
                 .then(response => {
                     setSearchResult(response);
+                    setLoading(false);
                 })
         }
+    }, [keyword])
+
+    const delayedSearch = useCallback(debounce(updateSearch, 500),[keyword]);
+
+    useEffect(() =>{
+        delayedSearch();
+        return delayedSearch.cancel;
     }, [keyword])
 
     return (
@@ -34,10 +46,13 @@ function SearchVideo({listID}) {
                 <div> 재생목록에 추가하고 싶은 영상은 <S.Like/>, 추가하고 싶지 않은 영상은 <S.Dislike/>을 눌러주세요. <br/> </div>
             </S.SearchComment>
             {!keyword && <RecommendVideoList listID={listID} username={username}/>}
-            {keyword && searchResult &&
-                <VideoList videoList={searchResult} checkplaylist={true}/>
+            {keyword && loading &&
+                <FallbackVideoList/>
             }
-            {keyword && (!searchResult || searchResult.length === 0) &&
+            {keyword && !loading && searchResult &&
+                <VideoList videoList={searchResult} checkplaylist={1}/>
+            }
+            {keyword && !loading && (!searchResult || searchResult.length === 0) &&
                 <S.InvalidSearchFeedback> 검색 결과가 없습니다. </S.InvalidSearchFeedback>
             }
         </S.SearchVideoWrapper>
